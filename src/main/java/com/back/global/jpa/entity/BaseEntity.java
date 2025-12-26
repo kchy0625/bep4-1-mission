b9cc8cb10e4a71ec047c3fe2a8efd895d7b9f59b
YoungE <56716209+kchy0625@users.noreply.github.com>
2025-12-26 15:20:08 +0900

    0027
    0027

--- src/main/java/com/back/global/jpa/entity/BaseEntity.java
+++ src/main/java/com/back/global/jpa/entity/BaseEntity.java
@@ -1,25 +1,25 @@
 package com.back.global.jpa.entity;
 
 import com.back.global.global.GlobalConfig;
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
 @Getter
-// 모든 엔티티들의 조상
 public abstract class BaseEntity {
+    public abstract int getId();
+
+    public abstract LocalDateTime getCreateDate();
+
+    public abstract LocalDateTime getModifyDate();
+
     public String getModelTypeCode() {
         return this.getClass().getSimpleName();
     }
+
     protected void publishEvent(Object event) {
         GlobalConfig.getEventPublisher().publish(event);
     }
-    public abstract int getId();
-    public abstract LocalDateTime getCreateDate();
-    public abstract LocalDateTime getModifyDate();
 }
